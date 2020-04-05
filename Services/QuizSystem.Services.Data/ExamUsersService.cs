5bad203461c590cdbed274dd251d8329b2a14ca1
Hristo Gilev <hristo_gilev@abv.bg>
2020-04-05 16:06:38 +0300

    CreateRole.cshtml, CreateRoleViewModel.cs, Create IExamUsersService.cs,ExamUsersService.cs
    Signed-off-by: Hristo Gilev <hristo_gilev@abv.bg>

---
+++ Services/QuizSystem.Services.Data/ExamUsersService.cs
@@ -0,0 +1,43 @@
+﻿namespace QuizSystem.Services.Data
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using QuizSystem.Data.Common.Repositories;
+    using QuizSystem.Data.Models;
+
+    public class ExamUsersService : IExamUsersService
+    {
+        private readonly IDeletableEntityRepository<Exam> repositoryExams;
+        private readonly IDeletableEntityRepository<ApplicationUser> repositoryUsers;
+        private readonly IDeletableEntityRepository<ExamUser> repository1;
+
+        public ExamUsersService(
+            IDeletableEntityRepository<Exam> repositoryExams,
+            IDeletableEntityRepository<ApplicationUser> repositoryUsers,
+            IDeletableEntityRepository<ExamUser> repository1)
+        {
+            this.repositoryExams = repositoryExams;
+            this.repositoryUsers = repositoryUsers;
+            this.repository1 = repository1;
+        }
+
+        public async Task AddUserToExamAsync(int idExam, string[] usersName)
+        {
+            foreach (var item in usersName)
+            {
+                var user = this.repositoryUsers.All().FirstOrDefault(t => t.UserName == item);
+                var exam = this.repositoryExams.All().FirstOrDefault(t => t.Id == idExam);
+                ExamUser examUser = new ExamUser() { UserId = user.Id, ExamId = exam.Id.ToString() };
+                await this.repository1.AddAsync(examUser);
+                await this.repository1.SaveChangesAsync();
+                exam.ExamUser.Add(examUser);
+                this.repositoryExams.Update(exam);
+                await this.repositoryExams.SaveChangesAsync();
+                user.ExamUser.Add(examUser);
+                this.repositoryUsers.Update(user);
+                await this.repositoryUsers.SaveChangesAsync();
+            }
+        }
+    }
+}
---
+++ Web/QuizSystem.Web/Areas/ArreasModels/RoleModels/CreateRoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuizSystem.Web.Areas.ArreasModels.RoleModels
+{
+    public class CreateRoleViewModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+    }
+}
